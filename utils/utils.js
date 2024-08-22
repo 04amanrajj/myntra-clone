51d3e8c8adf032096d088216bbe23631ef9fa895
04amanrajj <04.aman.raj@gmail.com>
2024-08-23 01:22:59 +0530

    added a function to store user's name

--- utils/utils.js
+++ utils/utils.js
@@ -19,18 +19,28 @@ const tostTopEnd = Swal.mixin({
 });
 
 const tostBottomEnd = Swal.mixin({
-    toast: true,
-    position: "bottom-end",
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.onmouseenter = Swal.stopTimer;
-      toast.onmouseleave = Swal.resumeTimer;
-    },
+  toast: true,
+  position: "bottom-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  },
+});
+
+// authantication
+function isUserLoggedin() {
+  document.addEventListener("DOMContentLoaded", function () {
+    let loggedInUser = localStorage.getItem("loggedInUser");
+    if (loggedInUser) {
+      let loginLink = document.querySelector(".username");
+      if (loginLink) {
+        loginLink.textContent = loggedInUser;
+      }
+    }
   });
-  
+}
 
-export { tostTopEnd ,tostBottomEnd,filter,search };
-
-
+export { tostTopEnd, tostBottomEnd, filter, search, isUserLoggedin };
