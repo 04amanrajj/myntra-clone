bb3f3c1f8582b4e6079a4cff1fb9a6e233a983a2
04amanrajj <04.aman.raj@gmail.com>
2024-08-23 19:26:24 +0530

    women's data added and fixed css

---
+++ scripts/women-page.js
@@ -0,0 +1,51 @@
+let images = [
+  "../images/slideshow/menPage/Workwear_Desk.jpg",
+  "../images/slideshow/menPage/Sports-Shoes_Desk2.jpg",
+  "../images/slideshow/menPage/Lancer_Desk.jpg",
+  "../images/slideshow/menPage/Sports-Shoes_Desk.jpg",
+  "../images/slideshow/menPage/CR7_Desk_Baner.jpg",
+  "../images/slideshow/menPage/Backpacks---Luggage_Desk.jpg",
+];
+
+function slideshow(images) {
+  let i = 0;
+  let interval = false;
+
+  let imgdiv = document.querySelector(".slideshow");
+
+  function updateimg() {
+    imgdiv.innerHTML = "";
+    let pic = document.createElement("img");
+    pic.src = images[i];
+    pic.setAttribute("id", "slideimg");
+    imgdiv.append(pic);
+    pic.addEventListener(
+      "click",
+      () => (window.location.href = "women-item.html")
+    );
+  }
+  if (!interval) {
+    interval = setInterval(() => {
+      i++;
+      if (i == images.length) i = 0;
+      updateimg();
+    }, 5000);
+  }
+  updateimg();
+}
+
+slideshow(images);
+
+// redirect to mens page on img click
+let bottomImages = document.querySelectorAll("#Main .bimgs");
+bottomImages.forEach((image) => {
+  image.addEventListener(
+    "click",
+    () => (window.location.href = "women-item.html")
+  );
+});
+
+
+// user name show in navbar
+import { isUserLoggedin } from '../utils/utils.js';
+isUserLoggedin()
